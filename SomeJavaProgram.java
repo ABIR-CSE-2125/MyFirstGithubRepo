ffc9fe3be00d315b075aa9f8fc664cc54fe2ecf2
Abir Bera <abirbera2003@gmail.com>
2023-07-16 21:38:04 +0530

    Successful 001

---
+++ SomeJavaProgram.java
@@ -0,0 +1,7 @@
+package MyFirstGitRepo;
+
+public class SomeJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("It's my first git operation on any file or folder!!");
+    }
+}
